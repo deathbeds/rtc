51a22d001ff931eadfac2c9eef6476e40d29977f
tonyfast <tony.fast@gmail.com>
2021-02-03 14:47:22 -0500

    make a binder

---
+++ dodo.py
@@ -0,0 +1,11 @@
+def task_jupyter_server_config():
+    def make_config():
+        import pathlib, json
+        p = pathlib.Path("automerge/jupyter_server_config.py")
+        d = json.loads(p.read_text())
+        d["ServerApp"]["allow_origin"] = "*"
+        pathlib.Path("jupyter_server_config.py").write_text(
+            json.dumps(d)
+        )
+
+    return dict(actions=[make_config])
